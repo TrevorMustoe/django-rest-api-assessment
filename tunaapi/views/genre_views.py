9b1548e7f3c9e539945a379d1321c4d038cd7e23
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-12-07 10:44:42 -0500

    Get on Genre and single genre by ID

---
+++ tunaapi/views/genre_views.py
@@ -0,0 +1,37 @@
+"""View module for handling requests about genre"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Genre
+
+
+class GenreView(ViewSet):
+    """Tuna Api Genre view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single Genre
+
+        Returns:
+            Response -- JSON serialized Genre
+        """
+        genre = Genre.objects.get(pk=pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all genres
+
+        Returns:
+            Response -- JSON serialized list of genres
+        """
+        
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data)   
+class GenreSerializer(serializers.ModelSerializer):
+    """JSON serializer for genre
+    """
+    class Meta:
+        model = Genre
+        fields = ('id', 'description')
--- tunapiano/urls.py
+++ tunapiano/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from tunaapi.views import ArtistsView
+from tunaapi.views import ArtistsView, GenreView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'artists', ArtistsView, 'artists')
+router.register(r'genre', GenreView, 'genre')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
